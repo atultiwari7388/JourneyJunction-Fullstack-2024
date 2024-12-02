9fb63961e60da31c167ae0ab7be78ff40c36f31b
Atul Tiwari <tiwariatul9526@gmail.com>
2024-12-02 19:23:09 +0530

    1. Db Setup

---
+++ backend/server.ts
@@ -0,0 +1,11 @@
+import app from "./src/app";
+
+const startServer = async () => {
+  const port = process.env.PORT || 4000;
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
